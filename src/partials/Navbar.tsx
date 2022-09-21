dc155f2c369a94d5feb82c16f6c070ce812110cf
Nischay Sharma <nischay876@gmail.com>
2022-09-22 02:04:12 +0530

    Update Navbar.tsx

--- src/partials/Navbar.tsx
+++ src/partials/Navbar.tsx
@@ -11,31 +11,16 @@ const Navbar = () => (
     <NavbarTwoColumns>
       <a href="/">
         <Logo
-          icon={
-            <svg
-              className="mr-1 h-10 w-10 stroke-cyan-600"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M0 0h24v24H0z" stroke="none"></path>
-              <rect x="3" y="12" width="6" height="8" rx="1"></rect>
-              <rect x="9" y="8" width="6" height="12" rx="1"></rect>
-              <rect x="15" y="4" width="6" height="16" rx="1"></rect>
-              <path d="M4 20h14"></path>
-            </svg>
-          }
-          name="Ixartz's Blog"
+          icon={""}
+          name="Nischay's Blog"
         />
       </a>
 
       <NavMenu>
+        <NavMenuItem href="/">Home</NavMenuItem>
         <NavMenuItem href="/posts">Blogs</NavMenuItem>
-        <NavMenuItem href="/">GitHub</NavMenuItem>
-        <NavMenuItem href="/">Twitter</NavMenuItem>
+        <NavMenuItem href="https://github.com/nischay876">GitHub</NavMenuItem>
+        <NavMenuItem href="https://dsc.gg/oggys">Discord</NavMenuItem>
       </NavMenu>
     </NavbarTwoColumns>
   </Section>
