ac40d781ccce5176c1adb2f1eedbe9371b94c330
Nischay Sharma <nischay876@gmail.com>
2022-09-22 02:04:42 +0530

    Update ProjectList.tsx

--- src/partials/ProjectList.tsx
+++ src/partials/ProjectList.tsx
@@ -9,28 +9,30 @@ import {
 const ProjectList = () => (
   <Section
     title={
-      <>
-        Recent <GradientText>Projects</GradientText>
-      </>
+      <div className="flex items-baseline justify-between">
+        <div>
+          Recent <GradientText>Projects</GradientText>
+        </div>
+
+        <div className="text-sm">
+          <a href="https://github.com/nischay876?tab=repositories">View all Projects →</a>
+        </div>
+      </div>
     }
   >
     <div className="flex flex-col gap-6">
       <Project
-        name="Project 1"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        bibendum. Nunc non posuere consectetur, justo erat semper enim, non
-        hendrerit dui odio id enim."
-        link="/"
+        name="iimg.ml"
+        description="Free images sharing service with Zero-Width Spaces share links, Custom domain, Never expire and alot of Customisations..."
+        link="https://docs.iimg.ml"
         img={{
-          src: '/assets/images/project-web-design.png',
+          src: 'https://i.imgur.com/FFV6F3C.png',
           alt: 'Project Web Design',
         }}
         category={
           <>
-            <Tags color={ColorTags.FUCHSIA}>Astro.js</Tags>
-            <Tags color={ColorTags.LIME}>Web design</Tags>
-            <Tags color={ColorTags.SKY}>Tailwind.css</Tags>
-            <Tags color={ColorTags.ROSE}>TypeScript</Tags>
+            <Tags color={ColorTags.FUCHSIA}>Image Sharing</Tags>
+            <Tags color={ColorTags.YELLOW}>JavaScript</Tags>
           </>
         }
       />
