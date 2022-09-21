30e777bbd8639f803ec76018d5e3ce8c6964759c
Nischay Sharma <nischay876@gmail.com>
2022-09-22 02:05:35 +0530

    Update AppConfig.ts

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
-  title: 'Astro boilerplate',
-  description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Emma',
+  site_name: "Nischay's Blog",
+  title: "Nischay's Blog",
+  description: "Nischay Sharma's Blog Posting and Persional Website",
+  author: 'Nischay Sharma',
   locale_region: 'en-us',
   locale: 'en',
 };
